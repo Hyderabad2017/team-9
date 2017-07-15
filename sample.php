f7651eb15561d0497024fa976ccc13d8c04522c4
KOLLI HARIKA <harika.kolli3003@gmail.com>
2017-07-15 17:40:01 +0530

    notification sample

---
+++ sample.php
@@ -0,0 +1,55 @@
+<?php
+    /* Send an SMS using Twilio. You can run this file 3 different ways:
+     *
+     * 1. Save it as sendnotifications.php and at the command line, run 
+     *         php sendnotifications.php
+     *
+     * 2. Upload it to a web host and load mywebhost.com/sendnotifications.php 
+     *    in a web browser.
+     *
+     * 3. Download a local server like WAMP, MAMP or XAMPP. Point the web root 
+     *    directory to the folder containing this file, and load 
+     *    localhost:8888/sendnotifications.php in a web browser.
+     */
+
+    // Step 1: Get the Twilio-PHP library from twilio.com/docs/libraries/php, 
+    // following the instructions to install it with Composer.
+    require_once "vendor/autoload.php";
+   use Twilio\Rest\Client;
+    
+    // Step 2: set our AccountSid and AuthToken from https://twilio.com/console
+    $AccountSid = "ACd74e2681571e8a95cae78c09ea35fd1a";
+    $AuthToken = "ed7ce42d458d56ca172534e8c185b228";
+
+    // Step 3: instantiate a new Twilio Rest Client
+    $client = new Client($AccountSid, $AuthToken);
+
+    // Step 4: make an array of people we know, to send them a message. 
+    // Feel free to change/add your own phone number and name here.
+    $people = array(
+        "+918008030261" => "Curious George",
+		"+917659815633" => "lakshmi",
+       
+    );
+
+    // Step 5: Loop over all our friends. $number is a phone number above, and 
+    // $name is the name next to it
+    foreach ($people as $number => $name) {
+
+        $sms = $client->account->messages->create(
+
+            // the number we are sending to - Any phone number
+            $number,
+
+            array(
+                // Step 6: Change the 'From' number below to be a valid Twilio number 
+                // that you've purchased
+                'from' => "+13603017784", 
+                
+                'body' => "Hey we need you to kindly donate the blood at xxx blood bank"
+            )
+        );
+
+        // Display a confirmation message on the screen
+        echo "Sent message to $name";
+    }
