b45f6f26103930822c4720d7ab5cda340963a060
Manasa Susirla <manasa.susirla@gmail.com>
2017-07-15 21:16:58 +0530

    Add files via upload
    bootstrap css and js files

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer >
+    <div class="container">
+        <center>
+            <p>Copyright &copy; Ringel Store. All Rights Reserved  |  Contact Us: +91 90000 00000</p>	
+        </center>
+    </div>
+</footer>
